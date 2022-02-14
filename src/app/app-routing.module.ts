bd38ff948a01bd1034624fb405aae37cea207ec7
Carlosdizx <carlodiaz@umariana.edu.co>
2022-02-14 14:26:01 -0500

    Creado un componente con datos de prueba de un listado con angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ReportesComponent} from "./components/reportes/reportes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reportes', component: ReportesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReportesComponent } from './components/reportes/reportes.component';
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [AppComponent, ReportesComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
