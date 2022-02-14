b1d57582b404bb08efe19746b11e2ae3d3724f2d
Carlosdizx <carlodiaz@umariana.edu.co>
2022-02-14 14:42:06 -0500

    agregado en el angular.json el script para que tenga interactividad algunos componnetes de bootstrap, además, se creo un nuevo componente que contendra las rutas de navegacion (navbar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReportesComponent } from './components/reportes/reportes.component';
 import {MatTableModule} from "@angular/material/table";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, ReportesComponent],
+  declarations: [AppComponent, ReportesComponent, NavBarComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
