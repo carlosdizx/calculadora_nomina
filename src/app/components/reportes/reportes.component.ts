d1b7889e9d5391d3e6a95bf7abfa872a68e280e8
Carlosdizx <carlodiaz@umariana.edu.co>
2022-02-14 13:00:45 -0500

    creado la carpeta donde estarán los componentes, ademeas de que se creo uno, el de reportes

---
+++ src/app/components/reportes/reportes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reportes',
+  templateUrl: './reportes.component.html',
+  styleUrls: []
+})
+export class ReportesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
