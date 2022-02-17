4c9143c62b9dfaff57cffe986280d39a4414bea7
Carlosdizx <carlodiaz@umariana.edu.co>
2022-02-17 12:33:41 -0500

    Mejorado el codigo, para que este preparado para recibir respuestas dinamicas

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ReporteService } from '../../services/calculos.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {ReporteService} from '../../services/calculos.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
@@ -9,7 +9,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CalculadoraComponent implements OnInit {
   formulario: FormGroup;
-  mensajes: String[] = [];
+  mensajes: any = [];
+  mensaje: any = '';
 
   constructor(
     private service: ReporteService,
@@ -25,11 +26,26 @@ export class CalculadoraComponent implements OnInit {
     this.mensajes = [];
     const datos = this.formulario.value.semana.split('-');
     this.service
-      .horasTrabajadasPorSemana(this.formulario.value.tecnico, datos[0],datos[1].substring(1))
+      .horasTrabajadasPorSemana(
+        this.formulario.value.tecnico,
+        datos[0],
+        datos[1].substring(1)
+      )
       .subscribe((respuesta: any) => {
-        this.mensajes = Object.values(respuesta);
+        console.log(respuesta.mensaje);
+        if (typeof respuesta.mensaje === 'string') {
+          this.mensaje = respuesta.mensaje;
+          this.mensajes = [];
+        } else {
+          const resultado = Object.values(respuesta.mensaje);
+          this.mensaje = resultado[0];
+          this.mensajes = resultado[1];
+        }
+        //this.mensaje = arreglo[0];
+        //this.mensajes = Object.values(respuesta)[1];
       });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
--- src/app/services/calculos.service.ts
+++ src/app/services/calculos.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Reporte } from '../models/Reporte';
 
 @Injectable({
   providedIn: 'root',
