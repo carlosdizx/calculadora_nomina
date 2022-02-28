ad70c3cbf65516e83a92e06f1c4d0054befc8c82
Carlos <carlodiaz@umariana.edu.co>
2022-02-28 14:55:51 -0500

    agrega validación

--- src/app/components/reportes/form-reporte/form-reporte.component.ts
+++ src/app/components/reportes/form-reporte/form-reporte.component.ts
@@ -21,7 +21,7 @@ export class FormReporteComponent implements OnInit {
     private router: Router
   ) {
     this.formulario = this.formBuilder.group({
-      tecnico: ['', [Validators.required, Validators.minLength(3)]],
+      tecnico: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(15)]],
       servicio: ['', [Validators.required]],
       fecha_inicio: ['', [Validators.required]],
       fecha_finalizacion: ['', [Validators.required]],
